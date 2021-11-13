eab52d07d0c5529f5802cad34ba1774e0f8437fb
Jefferson Denilson <jefersond121212@gmail.com>
2021-11-13 17:37:17 -0300

    usuario cria conta

---
+++ spec/system/visitor_registers_as_user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe 'Visitor registers as user' do
+    it 'successfully' do
+        visit root_path
+        click_on 'Registrar como usuário'
+
+        fill_in 'user_email', with: 'test@company.com'
+        fill_in 'user_password', with: '123456789'
+        fill_in 'user_password_confirmation', with: '123456789'
+        click_on 'commit'
+
+        expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
+end
