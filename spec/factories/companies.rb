80126b198398f700aa57e2876c58a1e91fa83cc3
Hikari <66630725+Hikari-desuyoo@users.noreply.github.com>
2021-11-17 13:39:36 -0500

    Update spec/factories/companies.rb
    Co-authored-by: Henrique Morato <riquemorato@gmail.com>

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :company do
-    sequence(:cnpj) { |n| "#{n}#{n}.#{n}#{n}#{n}.#{n}#{n}#{n}/#{n}#{n}#{n}#{n}-#{n}#{n}" } # quebra galho
+    sequence(:cnpj) { |n| "#{n}#{n}.#{n}#{n}#{n}.#{n}#{n}#{n}/#{n}#{n}#{n}#{n}-#{n}#{n}" }
     sequence(:legal_name) { |n| "Empresa n#{n}" }
     sequence(:billing_email) { |n| "test#{n}@companymail.com" }
     sequence(:billing_address) { |n| "endereco numero da empresa numero #{n}" }
