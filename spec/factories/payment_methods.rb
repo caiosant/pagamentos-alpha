00da76c4263b2aed420d0e2653a21d0c91cd9cf3
Jefferson Denilson <jefersond121212@gmail.com>
2021-11-19 22:46:54 -0300

    adicionado factory com sequence para name

--- spec/factories/payment_methods.rb
+++ spec/factories/payment_methods.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :payment_method do
-    name { 'MyString' }
-    fee { '9.99' }
-    maximum_fee { '9.99' }
+    name { 'Cartão de Crédito Visa' }
+    fee { '5' }
+    maximum_fee { '50' }
+    icon { Rack::Test::UploadedFile.new('app/assets/images/icone_visa.jpg', 'image/jpg') }
+  end
+
+  factory :payment_methods, parent: :payment_method do
+    sequence(:name) { |n| "Payment #{n}" }
   end
 end
