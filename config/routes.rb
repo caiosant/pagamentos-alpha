f224c6ca9d326d76a1d29f4b9fbfd23e59b06ce2
Alesandro Silva <silva_alesandro@outlook.com>
2021-11-21 20:17:05 -0400

    estiliza formulario de criar pagamentos

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   devise_for :admins
     
-namespace :admin do
-  get 'dashboard', to: 'dashboard#index', as: '/dashboard'
+  namespace :admin do
+    get 'dashboard', to: 'dashboard#index', as: '/dashboard'
 
-  resources :payment_methods, only: %i[new create show destroy index], shallow: true do
-    post 'enable', on: :member
-    post 'disable', on: :member
+    resources :payment_methods, only: %i[new create show destroy index], shallow: true do
+      post 'enable', on: :member
+      post 'disable', on: :member
+    end
   end
-end
-
-
 
   root 'home#index'
   resources :companies, only: %i[edit update show]
