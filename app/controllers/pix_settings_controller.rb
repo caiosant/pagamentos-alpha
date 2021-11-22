6a5c3dda9af934983482869ecdc8521ef7e8d1d0
Jefferson Denilson <jefersond121212@gmail.com>
2021-11-21 22:36:05 -0300

    criando configurações de pagamento para pix

--- app/controllers/pix_settings_controller.rb
+++ app/controllers/pix_settings_controller.rb
@@ -1,9 +1,23 @@
 class PixSettingsController < ApplicationController
+  before_action :authenticate_user!
+
   def new
     @payment_methods_dropdown = PaymentMethod.payment_methods_by_type_dropdown('pix')
     @pix_setting = PixSetting.new
   end
 
   def create
+    @pix_setting = PixSetting.new(**pix_params, company: current_user.company)
+
+    if @pix_setting.save
+      redirect_to company_payment_settings_path(current_user.company), notice: t('payment_settings.created_notice')
+    else
+      render :new
+    end
+  end
+
+  private
+  def pix_params
+    params.require(:pix_setting).permit(:pix_key, :bank_code, :payment_method_id)
   end
 end
--- spec/system/payment_setting/owner_creates_pix_setting_spec.rb
+++ spec/system/payment_setting/owner_creates_pix_setting_spec.rb
@@ -1,8 +1,35 @@
 require 'rails_helper'
 
 describe 'Owner creates pix payment setting' do
-    it 'successfully'
+    it 'successfully' do
+      owner = create(:user, :complete_company_owner)
+      company = owner.company
+      company.accepted!
+
+      pix_method, pix_method2, pix_method3  = create_list(:payment_method, 3, :pix)
+
+      login_as owner, scope: :user
+      visit company_path owner.company
+      click_on 'Meios de pagamento configurados'
+      click_on 'Configurar novo pix'
+
+      fill_in 'Chave PIX', with: '317283472634723'
+      fill_in 'Código do banco', with: '245'
+      select pix_method.name, from: 'Meio de pagamento'
+      click_on 'Criar Pagamento PIX'
+
+      expect(current_path).to eq(company_payment_settings_path owner.company)
+      expect(page).to have_content('Pagamento configurado com sucesso')
+      expect(page).to have_content('Chave PIX: 317283472634723')
+      expect(page).to have_content('Código do banco: 245')
+      expect(owner.company.payment_settings).to include(PixSetting.first)
+    end
+
     it 'but fails when entering invalid pix key(regex validation)'
     it 'but fails when entering non existing bank code'
     it 'but fails when leaving everything blank'
 end
+
+# ('Chave PIX')
+#     expect(page).to have_content('Código do banco')
+#     expect(page).to have_content('Meio de pagamento')
