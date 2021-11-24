65996cb1c56b928117a1f48a2673491cf22355cf
Caio Sant'Ana da Silva <santanacaiio@gmail.com>
2021-11-24 15:22:48 -0300

    adding first tests

--- spec/requests/api/customer_choose_payment_method_spec.rb
+++ spec/requests/api/customer_choose_payment_method_spec.rb
@@ -5,7 +5,6 @@ require 'rails_helper'
 # retorna token do cliente
 describe 'Customer API' do
   context 'POST /api/v1/customer' do
-
     context 'successfully' do
       it 'with pix' do
         owner = create(:user, :complete_company_owner)
@@ -67,18 +66,85 @@ describe 'Customer API' do
       end
     end
 
-    context 'with error' do
-      it 'should inform credit card number'
+    context '400 error' do
+      it 'should inform customer name' do
+        owner = create(:user, :complete_company_owner)
+        owner.company.accepted!
+        credit_card_method = create(:payment_method, :credit_card)
+        credit_card_setting = create(:credit_card_setting, company: owner.company, payment_method: credit_card_method)
+        company_payment_method, = owner.company.list_payment_methods
 
-      it 'should inform credit card expiration date'
+        customer_params = {
+          company_token: owner.company.token,
+          customer_name: '',
+          cpf: '111.111.111-11',
+          payment_method_token: company_payment_method.token,
+          credit_card_number: '47384876346537',
+          credit_card_expiration_date: 3.month.from_now
+        }
 
-      it 'should inform cpf'
+        post '/api/v1/customer', params: { customer: customer_params }
 
-      it 'should inform payment_method_id'
+        expect(response).to have_http_status(400)
+        expect(response.parsed_body[:message]). to eq('Nome do cliente deve ser enviado')
+      end
 
-      it 'payment_method does not exist'
+      it 'should inform CPF' do
+        owner = create(:user, :complete_company_owner)
+        owner.company.accepted!
+        credit_card_method = create(:payment_method, :credit_card)
+        credit_card_setting = create(:credit_card_setting, company: owner.company, payment_method: credit_card_method)
+        company_payment_method, = owner.company.list_payment_methods
 
-      it 'cannot create without company token'
+        customer_params = {
+          company_token: owner.company.token,
+          cpf: '',
+          payment_method_token: company_payment_method.token,
+          credit_card_number: '47384876346537',
+          credit_card_expiration_date: 3.month.from_now
+        }
+
+        post '/api/v1/customer', params: { customer: customer_params }
+
+        expect(response).to have_http_status(400)
+        expect(response.parsed_body[:message]). to eq('CPF deve ser enviado')
+      end
+
+      it 'should inform credit card number' do
+        owner = create(:user, :complete_company_owner)
+        owner.company.accepted!
+        credit_card_method = create(:payment_method, :credit_card)
+        credit_card_setting = create(:credit_card_setting, company: owner.company, payment_method: credit_card_method)
+        company_payment_method, = owner.company.list_payment_methods
+
+        customer_params = {
+          company_token: owner.company.token,
+          cpf: '111.111.111-11',
+          payment_method_token: company_payment_method.token,
+          credit_card_number: '',
+          credit_card_expiration_date: 3.month.from_now
+        }
+
+        post '/api/v1/customer', params: { customer: customer_params }
+
+        expect(response).to have_http_status(400)
+        expect(response.parsed_body[:message]). to eq('Número do cartão de crédito deve ser enviado')
+      end
+
+      it 'should inform credit card expiration date' do
+      end
+
+      it 'should inform cpf' do
+      end
+
+      it 'should inform payment_method_id' do
+      end
+
+      it 'payment_method does not exist' do
+      end
+
+      it 'cannot create without company token' do
+      end
     end
 
   end
