942c9b1247864ccda3d0a4866158d644f15765f8
Alesandro Silva <silva_alesandro@outlook.com>
2021-11-24 10:55:32 -0400

    cria teste para troca de status

---
+++ spec/system/user/user_changes_product_status_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'User changes products status' do
+  it 'to disabled successfully' do
+    user = create(:user, :complete_company_owner)
+    user.company.accepted!
+    product = create(:product, company: user.company)
+
+    login_as user, scope: :user
+    visit product_path(product)
+    click_on 'Desabilitar'
+
+    expect(page).to have_content('Desabilitado')
+    expect(page).not_to have_content('Habilitado')
+    expect(product.reload.status).to eq 'disabled'
+  end
+
+  it 'to enabled successfully' do
+    user = create(:user, :complete_company_owner)
+    user.company.accepted!
+    product = create(:product, company: user.company)
+    product.disabled!
+
+    login_as user, scope: :user
+    visit product_path(product)
+    click_on 'Habilitar'
+
+    expect(page).to have_content('Habilitado')
+    expect(page).not_to have_content('Desabilitado')
+    expect(product.reload.status).to eq 'disabled'
+  end
+end
