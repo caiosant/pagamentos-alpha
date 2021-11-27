79f103d5c211a85934da275c48561602a69234c1
Jefferson Denilson <jefersond121212@gmail.com>
2021-11-26 22:24:13 -0300

    associando customer com credit card com sucesso

--- app/models/customer_payment_method.rb
+++ app/models/customer_payment_method.rb
@@ -4,4 +4,17 @@ class CustomerPaymentMethod < ApplicationRecord
   belongs_to :customer
 
   after_create :generate_token_attribute
+
+  # TODO: validar expiration date
+  validates :credit_card_name, :credit_card_number, :credit_card_expiration_date,
+    :credit_card_security_code, presence: true, if: -> { payment_method.credit_card? }
+
+  def add_credit_card(params)
+    return unless payment_method.credit_card?
+
+    self.credit_card_name = params[:credit_card_name]
+    self.credit_card_number = params[:credit_card_number]
+    self.credit_card_expiration_date = params[:credit_card_expiration_date]
+    self.credit_card_security_code = params[:credit_card_security_code]
+  end
 end
--- spec/requests/api/customer_choose_payment_method_spec.rb
+++ spec/requests/api/customer_choose_payment_method_spec.rb
@@ -70,26 +70,35 @@ describe 'CustomerPaymentMethod API' do
       it 'with credit card' do
         owner = create(:user, :complete_company_owner)
         owner.company.accepted!
+        customer = create(:customer, company: owner.company)
         credit_card_method = create(:payment_method, :credit_card)
         credit_card_setting = create(:credit_card_setting, company: owner.company, payment_method: credit_card_method)
-        company_payment_method = owner.company.list_payment_methods
+        company_payment_setting, = owner.company.payment_settings
 
         allow(SecureRandom).to receive(:alphanumeric).with(20).and_return('hPxFizxVM5p5mNpFdOsf')
 
-        customer_params = {
-          cpf: '111.111.111-11',
-          payment_method_token: company_payment_method.token,
-          credit_card_number: '47384876346537',
-          credit_card_expiration_date: 3.month.from_now
+        customer_payment_method_params = {
+          customer_payment_method: {
+            customer_token: customer.token,
+            payment_method_token: company_payment_setting.token,
+            credit_card_name: 'Credit Card 1',
+            credit_card_number: '4929513324664053',
+            credit_card_expiration_date: 3.months.from_now,
+            credit_card_security_code: '123'
+          }
         }
+        post '/api/v1/customer_payment_method',
+          params: customer_payment_method_params,
+          headers: { 'companyToken' => owner.company.token }
 
-        post '/api/v1/customer_payment_method', params: {
-          company_token: owner.company.token,
-          customer: customer_params
-        }
-
-        expect(response).to have_http_status(200)
-        expect(parsed_body[:customer_token]).to eq('hPxFizxVM5p5mNpFdOsf')
+        customer_payment_method = parsed_body[:customer_payment_method]
+        expect(response).to have_http_status(201)
+        expect(CustomerPaymentMethod.count).to eq(1)
+        expect(customer_payment_method[:token]).to eq('hPxFizxVM5p5mNpFdOsf')
+        expect(customer_payment_method[:payment_method][:name]).to eq(credit_card_method.name)
+        expect(customer_payment_method[:payment_method][:type_of]).to eq(credit_card_method.type_of)
+        expect(customer_payment_method[:customer][:token]).to eq(customer.token)
+        expect(customer_payment_method[:company][:legal_name]).to eq(owner.company.legal_name)
       end
     end
 
