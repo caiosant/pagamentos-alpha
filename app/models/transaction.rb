f78e3fee5d70017034dacb930882e1a3731db3db
Hikari-desuyoo <andre123luiz.al@gmail.com>
2021-11-29 17:34:18 -0300

    Adicionado optional nas configuracoes das cobrancas

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,8 +1,8 @@
 class Transaction < ApplicationRecord
   belongs_to :customer_payment_method
-  belongs_to :pix_setting
-  belongs_to :boleto_setting
-  belongs_to :credit_card_setting
+  belongs_to :pix_setting, optional: true
+  belongs_to :boleto_setting, optional: true
+  belongs_to :credit_card_setting, optional: true
   belongs_to :product
   belongs_to :receipt
   enum type_of: { pix: 0, boleto: 5, credit_card: 10}
