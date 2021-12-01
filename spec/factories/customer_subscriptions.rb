5235095c170fea720cdc75c66c19717798df11ed
Jefferson Denilson <jefersond121212@gmail.com>
2021-12-01 20:54:41 -0300

    factories de customer_subscriptions

--- spec/factories/customer_subscriptions.rb
+++ spec/factories/customer_subscriptions.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :customer_subscription do
-    token { 'MyString' }
-    status { 1 }
-    cost { '9.99' }
+    company { create(:company, status: :accepted) }
+    status { 'active' }
+    cost { 9.99 }
     renovation_date { '2021-11-30' }
-    product { nil }
-    customer_payment_method { nil }
+    product { create(:product, type_of: 'subscription', company: company) }
+    customer_payment_method { create(:customer_payment_method, :pix, company: company) }
   end
 end
