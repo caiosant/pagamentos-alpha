2aeff5f83dafa29b043b12f0b0b7ae27c9eb43eb
Jefferson Denilson <jefersond121212@gmail.com>
2021-12-01 21:40:51 -0300

    criando purchases no dia de renovação da assinatura

--- spec/models/customer_subscription_spec.rb
+++ spec/models/customer_subscription_spec.rb
@@ -5,12 +5,19 @@ RSpec.describe CustomerSubscription, type: :model do
 
   context '.renew_subscriptions' do
     it 'should create purchases for subscriptions in 2021-12-01' do
-      customer_subscription_1, customer_subscription_2 = create_list(
-        :customer_subscription, 2, renovation_date: '2021-12-01'
-      )
-      another_customer_subscription = create(:customer_subscription, renovation_date: '2021-12-02')
-
+      customer_subscription_1 = customer_subscription_2 = another_customer_subscription = nil
+      
       travel_to Date.new(2021, 12, 01) do
+        customer_subscription_1, customer_subscription_2 = create_list(
+          :customer_subscription, 2
+        )
+      end
+
+      travel_to Date.new(2021, 12, 02) do
+        another_customer_subscription = create(:customer_subscription)
+      end
+
+      travel_to Date.new(2022, 01, 01) do
         CustomerSubscription.renew_subscriptions
 
         expect(Purchase.count).to eq(2)
@@ -21,8 +28,7 @@ RSpec.describe CustomerSubscription, type: :model do
         )
         expect(first_purchase.product.token).to eq(customer_subscription_1.product.token)
         expect(first_purchase.cost).to eq(customer_subscription_1.cost)
-        # expiration date deveria ser o mesmo do renovation date?
-        expect(first_purchase.expiration_date).to eq(customer_subscription_1.renovation_date)
+        expect(first_purchase.expiration_date.day).to eq(customer_subscription_1.renovation_date)
         expect(first_purchase.company.id).to eq(customer_subscription_1.company.id)
 
         expect(second_purchase.customer_payment_method.token).to eq(
@@ -30,13 +36,15 @@ RSpec.describe CustomerSubscription, type: :model do
         )
         expect(second_purchase.product.token).to eq(customer_subscription_2.product.token)
         expect(second_purchase.cost).to eq(customer_subscription_2.cost)
-        expect(second_purchase.expiration_date).to eq(customer_subscription_2.renovation_date)
+        expect(second_purchase.expiration_date.day).to eq(customer_subscription_2.renovation_date)
         expect(second_purchase.company.id).to eq(customer_subscription_2.company.id)
       end
     end
 
     it 'should only create purchases for active subscriptions'
 
+    it 'should not create purchase at same day subscription is created'
+    
     it 'should create purchases for subscriptions not renewed in the right day'
   end
 
